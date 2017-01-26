9ff0d5c05c111864ff460cc1d29c4b1740e79000
“KatyaSharko“ <katyasharko@gmail.com>
2017-01-26 20:02:29 +0200

    add table and classes to displaying time

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,43 @@
 'use strict';
 
-var gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp');
 
 gulp.task('styles', function() {
+    var cleanCSS = require('gulp-clean-css'),
+        concat = require('gulp-concat');
+    return gulp.src('src/css/*.css')
+        .pipe(concat('main.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('styles'));
 
 });
 
 gulp.task('build-js', function() {
+    var uglify = require('gulp-uglify'),
+        concat = require('gulp-concat');
     return gulp.src('src/scripts/*.js')
-        .pipe(sourcemaps.init())
-
-
-
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('scripts/'));
 });
 
 gulp.task('default', ['styles', 'build-js'], function() {
+    var livereload = require('gulp-livereload'),
+        open = require('gulp-open'),
+        options = {
+            uri: 'http://localhost:5000',
+            app: 'chrome'
+        };
+
+    livereload.listen({
+        start: true,
+        port: 35729
+    });
+
+    gulp.watch('src/css/*.css', ['styles']);
+    gulp.watch('src/scripts/*.js', ['build-js']);
+    gulp.watch(['styles/main.css', 'scripts/main.js']).on('change', livereload.changed);
+
+    gulp.src('index.html').pipe(open(options));
 
 });
---
+++ src/scripts/showTime.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ksharko on 26.01.2017.
+ */
+console.log('time here');
